16d6c7461da0dd784592e16e6928b47630e02e51
riekesetiawati <riekesetiawati@apps.ipb.ac.id>
2023-12-01 21:19:13 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect('https://your-app-name.up.railway.app/telsalfix/');
-
+    return view('welcome');
 });
